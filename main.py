8df9f78b815dc6cc38ca3368a1fe83d6b4cdd92f
Sruthi <sruthipedakolimi@gmail.com>
2026-06-05 12:01:12 -0500

    Fast API upload file working test

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi import UploadFile
+from pypdf import PdfReader
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {"message": "SmartApply is running"}
+
+@app.post('/upload')
+async def upload_file(file: UploadFile):
+    """
+        It accepts the single pdf file and returns the content
+    """
+    text = ""
+    # read the file content
+    content = PdfReader(file.file)
+    for page in content.pages:
+        text += page.extract_text()
+    
+    return text
