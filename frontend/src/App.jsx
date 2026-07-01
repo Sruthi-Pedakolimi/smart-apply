6ca65eb3329b755268e785a1667cf8fcc919c1dc
Sruthi <sruthipedakolimi@gmail.com>
2026-06-30 19:24:03 -0500

    Error handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,31 +6,57 @@ const SmartApply = () => {
   const [job_summary, updateSummary] = useState("")
   const [cover_letter, updateCoverLetter] = useState("")
   const [ats_score, updateATSScore] = useState("")
+  const [error, updateError] = useState("")
+  const [isLoading, updateIsLoading] = useState(false)
 
   const handleFileChange = (event) => {
         updateFile(event.target.files[0])
   }
 
   const handleJobSummaryChange = (event) => {
-      if (event.target.value != "") {
         updateSummary(event.target.value)
-      }
   }
 
   const handleSubmit = async (event) =>{
-    console.log("submitted successfully")
+
+    if (job_summary.trim() == ""){
+      updateError("Please fill job summary")
+      return
+    }
+    if (!file){
+       updateError("Please Upload file")
+      return
+    }
+
     const url = "http://localhost:8000/info"
     const formData = new FormData();
     formData.append("file", file);
     formData.append("job", job_summary);
-
+    updateError("")
+    updateIsLoading(true)
     const response = await fetch(url, {
       method: 'POST',
       body: formData
     })
+    if (!response.ok){
+      updateIsLoading(false)
+      return
+    }
+ 
     const data = await response.json()
+    updateIsLoading(false)
     updateCoverLetter(data.cover_letter)
     updateATSScore(data.ats_score)
+  }
+
+  const formatOutput = () => {
+      return (
+        <div>
+          <label className="label-heading">Output</label>
+          <p>{ats_score}</p>
+          <p>{cover_letter}</p>
+       </div> 
+      )
   }
 
   return (
@@ -50,11 +76,9 @@ const SmartApply = () => {
           Submit
        </button>
        </div>
-       <div>
-          <label className="label-heading">Output</label>
-          <p>{ats_score}</p>
-          <p>{cover_letter}</p>
-       </div>
+        {isLoading && <p>Generating.....</p>}
+        {error && <p>{error}</p>} 
+        {cover_letter && formatOutput()}   
     </div>
     </div>
 
